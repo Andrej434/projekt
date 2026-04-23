4059f06fca0ecd31a33b33b598da8009db07402a
Maximilian-M-09 <maximilianmelchioris@gmail.com>
2026-04-23 10:18:36 +0200

    Finalizacia
    pridanie register a v pripade nespravnych udajov vypise chybu, a v pripade registracie pomocou uz existujuceho pouzivatela takisto vybehne chyba

--- index.php
+++ index.php
@@ -15,21 +15,37 @@ ini_set('display_errors', 1);
 session_start();
 $conn = new mysqli("localhost", "root", "root", "php");
 
+if (isset($_POST["register"])) {
+    $_SESSION["register"] = 1;
+    header("Location: " . $_SERVER["PHP_SELF"]);
+    exit;
+}
+if (isset($_POST["return"])) {
+    unset($_SESSION["register"]);
+    header("Location: " . $_SERVER["PHP_SELF"]);
+    exit;
+}
+
 if ($conn->connect_error) {
     die("DB chyba: " . $conn->connect_error);
 }
 
 if (isset($_POST["login"])) {
     $name = $_POST["name"];
-    $password = $_POST["password"]; 
-    
-    $sql = "SELECT * FROM pouzivatelia WHERE Meno = '$name' AND Heslo = '$password'";
+    $password = $_POST["password"];
+
+    $sql = "SELECT * FROM pouzivatelia WHERE Meno='$name' AND Heslo='$password'";
     $result = mysqli_query($conn, $sql);
 
     if ($row = mysqli_fetch_assoc($result)) {
         $_SESSION["logged"] = 1;
         $_SESSION["id"] = $row["pouzivatel_id"];
         $_SESSION["meno"] = $row["Meno"];
+
+        header("Location: " . $_SERVER["PHP_SELF"]);
+        exit;
+    } else {
+        echo '<div class="alert alert-danger">Neplatné meno alebo heslo!</div>';
     }
 }
 
@@ -41,8 +57,9 @@ if (isset($_POST["logout"])) {
 ?>
 
     <header>
-    <?php if (!isset($_SESSION["logged"])) { ?>
-    
+    <?php 
+    if (!isset($_SESSION["logged"])) {
+        if (!isset($_SESSION["register"])) { ?>
         <form method="POST">
             <div class="mb-3">
                 <label class="form-label">Prihlasovacie Meno</label>
@@ -53,8 +70,9 @@ if (isset($_POST["logout"])) {
                 <input type="password" class="form-control" name="password">
             </div>
             <button type="submit" class="btn btn-primary" name="login">Prihlásiť sa</button>
+            <button type="submit" class="btn btn-warning" name="register">Registrácia</button>  
         </form>
-    
+    <?php } ?>
     <?php } else { ?>
     
         <div>
@@ -69,6 +87,47 @@ if (isset($_POST["logout"])) {
     </header>
     <hr>
     <main>
+
+    <?php
+    if (isset($_SESSION["register"])) { ?>
+
+
+        <form method="POST">
+            <div class="mb-3">
+                <label class="form-label">Prihlasovacie Meno</label>
+                <input type="text" class="form-control" name="name">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Heslo</label>
+                <input type="password" class="form-control" name="password">
+            </div>
+            <button type="submit" class="btn btn-warning" name="register2">Registrovať sa</button>  
+            <button type="submit" class="btn btn-primary" name="return">Späť</button> 
+        </form>
+    <?php
+    }
+
+    if (isset($_POST["register2"])) {
+        $name = $_POST["name"];
+        $password = $_POST["password"];
+
+        $sql3 = "SELECT * FROM pouzivatelia WHERE Meno='$name'";
+        $resultCheck = mysqli_query($conn, $sql3);
+
+        if (mysqli_num_rows($resultCheck) > 0) {
+            echo '<div class="alert alert-danger">Poúživateľ už existuje!</div>';
+        } else {
+            $sql4 = "INSERT INTO pouzivatelia (Meno, Heslo) VALUES ('$name', '$password')";
+            if (mysqli_query($conn, $sql4)) {
+                echo '<div class="alert alert-success">Registrácia úspešná! Môžete se prihlásiť.</div>';
+                unset($_SESSION["register"]);
+            } else {
+                echo '<div class="alert alert-danger">Chyba při registraci: ' . mysqli_error($conn) . '</div>';
+            }
+        }
+    }
+    ?>
+    
     
     <?php if (isset($_SESSION["logged"])) {
     
